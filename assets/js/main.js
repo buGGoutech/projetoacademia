e333f1811816afc5b0413252d95c9433c5db5b1e
buGGoutech <128762908+buGGoutech@users.noreply.github.com>
2023-03-23 22:37:01 -0300

    feat: new website gym

---
+++ assets/js/main.js
@@ -0,0 +1,165 @@
+/*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
+
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
+}
+
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exists */
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+
+/*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll('.nav__link')
+
+const linkAction = () =>{
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+const scrollHeader = () =>{
+    const header = document.getElementById('header')
+    // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
+    this.scrollY >= 50 ? header.classList.add('scroll-header') 
+                       : header.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
+    
+const scrollActive = () =>{
+  	const scrollY = window.pageYOffset
+
+	sections.forEach(current =>{
+		const sectionHeight = current.offsetHeight,
+			  sectionTop = current.offsetTop - 58,
+			  sectionId = current.getAttribute('id'),
+			  sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+
+		if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+			sectionsClass.classList.add('active-link')
+		}else{
+			sectionsClass.classList.remove('active-link')
+		}                                                    
+	})
+}
+window.addEventListener('scroll', scrollActive)
+
+/*=============== SHOW SCROLL UP ===============*/ 
+const scrollUp = () =>{
+	const scrollUp = document.getElementById('scroll-up')
+    // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
+	this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+						: scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400
+})
+
+sr.reveal(`.home__data, .footer__container, .footer__group`)
+sr.reveal(`.home__img`, {delay: 700, origin: 'bottom'})
+sr.reveal(`.logos__img, .program__card, .pricing__card`, {interval: 100})
+sr.reveal(`.choose__img, .calculate__content`, {origin: 'left'})
+sr.reveal(`.choose__content, .calculate__img`, {origin: 'right'})
+
+/*=============== CALCULATE JS ===============*/
+const   calculateForm = document.getElementById('calculate-form'),
+        calculateCm = document.getElementById('calculate-cm'),
+        calculateKg = document.getElementById('calculate-kg'),
+        calculateMessage = document.getElementById('calculate-message')
+
+const calculateBmi = (e) =>{
+    e.preventDefault()
+
+    if(calculateCm.value ==- '' || calculateKg.value === ''){
+        calculateMessage.classList.remove('color-green')
+        calculateMessage.classList.add('color-red')
+
+        calculateMessage.textContent = 'Preencha a altura e o peso'
+
+        setTimeout(() =>{
+            calculateMessage.textContent = ''
+        }, 3000)
+    } else{
+        const cm = calculateCm.value /100,
+              kg = calculateKg.value,
+              imc = Math.round(kg / (cm*cm))
+
+        if(imc < 18.5){
+            calculateMessage.classList.add('color-green')
+            calculateMessage.textContent = `Seu IMC é ${imc} e você é considerado magro`
+        } else if(imc < 25){
+            calculateMessage.classList.add('color-green')
+            calculateMessage.textContent = `Seu IMC é ${imc} e você é saudavel`
+        } else{
+            calculateMessage.classList.add('color-green')
+            calculateMessage.textContent = `Seu IMC é ${imc} e você está acima do peso`
+        }
+
+        calculateCm.value = ''
+        calculateKg.value = ''
+
+        setTimeout(() =>{
+            calculateMessage.textContent = ''
+        }, 20000)
+
+    }
+}        
+
+calculateForm.addEventListener('submit', calculateBmi)
+
+
+
+/*=============== EMAIL JS ===============*/
+const contactForm = document.getElementById('contact-form'),
+      contactMessage = document.getElementById('contact-message'),
+      contactUser = document.getElementById('contact-user')
+
+const sendEmail = (e) => {
+    e.preventDefault()
+
+    if(contactUser.value === ''){
+        contactMessage.classList.remove('color-green')
+        contactMessage.classList.add('color-red')
+
+        contactMessage.textContent = 'Entre com um email'
+
+        setTimeout(() =>{
+            contactMessage.textContent = ''
+        }, 5000)
+    } else{
+        emailjs.sendForm('service_98qc60r', 'template_i7ze9ac', '#contact-form', 'o0JDs4Yq0J-subtXd')
+        .then(() => {
+            contactMessage.classList.add('color-green')
+            contactMessage.textContent = 'E-mail registrado com sucesso'
+
+            setTimeout(() =>{
+                contactMessage.textContent = ''
+            }, 5000)
+        })
+    }
+}      
+
+contactForm.addEventListener('submit', sendEmail)
